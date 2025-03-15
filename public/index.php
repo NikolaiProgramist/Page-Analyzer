35d7c360208c38fe25d7cef197479344a9391bde
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-15 18:30:49 +0300

    Add new uses and change / route
    Add DI Container, Slim\Views\Twig, Slim\Views\TwigMiddleware to the index.php.
    Also add processing of the main page template in / route.

--- public/index.php
+++ public/index.php
@@ -2,14 +2,23 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../src/templates');
+});
 
-$app->get('/', function (Request $request, Response $response) {
-    return $response->write('Hello! I work.');
+$app = AppFactory::createFromContainer($container);
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+$app->get('/', function (Request $request, Response $response): Response {
+    return $this->get(Twig::class)->render($response, 'index.html.twig');
 });
 
 $app->run();
