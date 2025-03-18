9c914a96484cf58542936fa6f9f17e0d19f605a3
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-18 21:24:40 +0300

    Add check() method for create check in the database. Also add getChecks() for getting all checks

--- src/Repositories/UrlRepository.php
+++ src/Repositories/UrlRepository.php
@@ -4,6 +4,7 @@ namespace Page\Analyzer\Repositories;
 
 use PDO;
 use Page\Analyzer\DAO\Url;
+use Page\Analyzer\DAO\Check;
 
 class UrlRepository
 {
@@ -79,5 +80,37 @@ class UrlRepository
         $stmt->execute();
 
         return $this->connection->lastInsertId();
+    }
+
+    public function check(int $id, string $createdAt): void
+    {
+        $sql = "INSERT INTO url_checks (url_id, created_at) VALUES (:url_id, :created_at)";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(':url_id', $id);
+        $stmt->bindParam(':created_at', $createdAt);
+        $stmt->execute();
+    }
+
+    public function getChecks(int $url_id): array
+    {
+        $sql = "SELECT * FROM url_checks WHERE url_id = :url_id";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(':url_id', $url_id);
+        $stmt->execute();
+        $checks = [];
+
+        while ($row = $stmt->fetch()) {
+            $check = new Check($url_id);
+            $check->setId($row['id']);
+//            $check->setStatus($row['status']);
+//            $check->setH1($row['h1']);
+//            $check->setTitle($row['title']);
+//            $check->setDescription($row['description']);
+            $check->setCreatedAt($row['created_at']);
+
+            $checks[] = $check;
+        }
+
+        return $checks;
     }
 }
