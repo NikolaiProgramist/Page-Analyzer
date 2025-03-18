94e5ca7cfe246cf41321f9b852906eee2c76f499
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-18 15:47:36 +0300

    Add creating new url_checks table if its not exists to the database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS urls (
     name varchar(255) UNIQUE NOT NULL,
     created_at timestamp NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint NOT NULL REFERENCES urls (id),
+    status_code int NOT NULL,
+    h1 varchar(50) NOT NULL,
+    title varchar(50) NOT NULL,
+    description varchar(255) NOT NULL,
+    created_at timestamp NOT NULL
+);
