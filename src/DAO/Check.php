120f4a122f55644a5ffc75127ec28f6f9c2ea3a5
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-18 21:15:00 +0300

    Add new Check DAO to the src/DAO/Check.php

---
+++ src/DAO/Check.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Page\Analyzer\DAO;
+
+class Check
+{
+    private int $id;
+    private int $urlId;
+    private int $status;
+    private string $h1;
+    private string $title;
+    private string $description;
+    private string $createdAt;
+
+    public function __construct(int $urlId)
+    {
+        $this->urlId = $urlId;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setUrlId(int $urlId): void
+    {
+        $this->urlId = $urlId;
+    }
+
+    public function setStatus(int $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function setH1(string $h1): void
+    {
+        $this->h1 = $h1;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setCreatedAt(string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUrlId(): int
+    {
+        return $this->urlId;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function getH1(): string
+    {
+        return $this->h1;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+}
