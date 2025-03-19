3ba90e6f17de55733225368a92789bff906cd67a
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-19 21:37:45 +0300

    Change count of churs for the h1 and title colomns with 255 to the database.sql

--- database.sql
+++ database.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id bigint NOT NULL REFERENCES urls (id),
     status_code int,
-    h1 varchar(50),
-    title varchar(50),
+    h1 varchar(255),
+    title varchar(255),
     description varchar(255),
     created_at timestamp NOT NULL
 );
